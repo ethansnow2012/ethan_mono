96795459329284192dbbb90d443fe385286a65ef
ethansnow <ethansnow2012@gmail.com>
2024-05-11 12:43:42 +0800

    feat: add gltf scene

--- apps/my_first_three_project_with_nuxt/components/TheExperience.vue
+++ apps/my_first_three_project_with_nuxt/components/TheExperience.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { Scene, PerspectiveCamera, Mesh, SphereGeometry, MeshBasicMaterial, WebGLRenderer } from "three"
 import { useWindowSize } from "@vueuse/core"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { AmbientLight } from "three"
 
 let renderer: WebGLRenderer
+let controls: OrbitControls
 const experience: Ref<HTMLCanvasElement | null> = ref(null)
 
 const { width, height } = useWindowSize()
@@ -11,9 +15,19 @@ const aspectRatio = computed(() => width.value / height.value)
 const scene = new Scene()
 
 const camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000)
-camera.position.set(0, 0, 4)
+camera.position.set(0, 0, 700)
 
 scene.add(camera)
+
+const gltfLoader = new GLTFLoader()
+
+const anbientLight = new AmbientLight(0xffffff, 0.5)
+scene.add(anbientLight)
+
+gltfLoader.load("/models/the_house_lost_at_sea/scene.gltf", (gltf) => {
+  scene.add(gltf.scene)
+  console.log("gltf", gltf)
+})
 
 const sphere = new Mesh(new SphereGeometry(1, 32, 32), new MeshBasicMaterial({ color: 0x008080 }))
 
@@ -32,6 +46,8 @@ function updateRenderer() {
 function setRenderer() {
   if (experience.value) {
     renderer = new WebGLRenderer({ canvas: experience.value })
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
     updateRenderer()
   }
 }
@@ -47,13 +63,31 @@ onMounted(() => {
 })
 
 const loop = () => {
+  controls.update()
   sphere.position.x += 0.01
   updateRenderer()
   requestAnimationFrame(loop)
 }
 </script>
 <template>
-  <div>
+  <div class="scene">
     <canvas ref="experience" />
+    <div class="scene-attribute">
+      Scene Attribution: <a href="https://sketchfab.com/3d-models/the-house-lost-at-sea-1b3b3b3b3b3b4">Michel Donze</a>
+    </div>
   </div>
 </template>
+<style scoped>
+.scene {
+  position: relative;
+}
+.scene-attribute {
+  color: white;
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
+}
+.scene-attribute a {
+  color: white;
+}
+</style>
