db82df19256bf1ec144c5b0310e5e7f9c32ec388
ethansnow <ethansnow2012@gmail.com>
2024-05-14 20:37:55 +0800

    feat: errror handle wrapper

--- packages/trying_express-zod-api/src/endpoint.ts
+++ packages/trying_express-zod-api/src/endpoint.ts
@@ -1,40 +1,8 @@
 import { z } from "zod";
 import { defaultEndpointsFactory } from "express-zod-api";
 import { authMiddleware } from "./middleware";
+import { withErrorHandleWrapper } from "./errors";
 
-type HandlerType = (args: any) => Promise<any>;
-
-function combineDecorators(...decorators: Array<(handler: HandlerType) => HandlerType>) {
-    return function(handler: HandlerType) {
-        return decorators.reduce((wrapped, decorator) => decorator(wrapped), handler);
-    };
-}
-
-function withErrorLogging(handler: HandlerType) {
-    return async function(args: any) {
-        try {
-            return await handler(args);
-        } catch (error) {
-            console.log(`Error in handler:`, error);
-            // Optionally, you could handle the error further, e.g., by throwing a specific HTTP error.
-            throw error; // rethrow the error if you want calling code to handle it as well.
-        }
-    };
-}
-function withEmailAlert(handler: HandlerType) {
-    return async function(args: any) {
-        try {
-            return await handler(args);
-        } catch (error) {
-            console.log(`Pretending to send email...`);
-            throw error; // rethrow the error if you want calling code to handle it as well.
-        }
-    };
-}
-
-
-// Using the compose function
-const withErrorHandleWrapper = combineDecorators(withEmailAlert, withErrorLogging);
 
 export const helloWorldEndpoint = defaultEndpointsFactory.build({
   method: "get", 
