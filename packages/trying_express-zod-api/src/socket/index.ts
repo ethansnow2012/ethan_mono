3ec9f0a371e88012b1b911e78a35d5ecae3f6bf5
ethansnow <ethansnow2012@gmail.com>
2024-05-15 10:06:06 +0800

    finish: setup socket.io

--- packages/trying_express-zod-api/src/socket/index.ts
+++ packages/trying_express-zod-api/src/socket/index.ts
@@ -2,38 +2,57 @@ import { createSimpleConfig } from "zod-sockets";
 import { ActionsFactory } from "zod-sockets";
 import { z } from "zod";
 import { Server } from "socket.io";
+import express from "express";
 
-const config = createSimpleConfig(); // shorthand for root namespace only
+const messageSchema = z.object({
+    msg: z.string(),
+})
+const config = createSimpleConfig(
+    {
+        emission: {
+          //enter_chat: { schema: z.tuple([userSchema]) },
+          //leave_chat: { schema: z.tuple([userSchema]) },
+          new_messages: {
+            schema: z.tuple([messageSchema]),
+          },
+        },
+        hooks: {
+            onConnection: async ({ logger, client }) => {
+                logger.debug("Connected user", client.id);
+            },
+            onDisconnect: async ({ logger, client }) => {
+                logger.debug("Disconnected user", client.id);
+            },
+        },
+      }
+); // shorthand for root namespace only
 const actionsFactory = new ActionsFactory(config);
 
-const onChat = actionsFactory.build({
-    // ns: "/", // optional, root namespace is default
-    event: "chat",
-    input: z.tuple([z.string()]),
-    handler: async ({ input: [message], client, all, withRooms, logger }) => {
-      /* your implementation here */
-      // typeof message === "string"
-    },
-  });
+
+
 const onPing = actionsFactory.build({
     event: "ping",
     input: z.tuple([]).rest(z.unknown()),
     output: z.tuple([z.literal("pong")]).rest(z.unknown()),
     handler: async ({ input }) => ["pong", ...input] as const,
 });
+
 const onMessage = actionsFactory.build({
     event: 'message',
     input: z.tuple([z.string()]),
-    handler: async ({ input }) => {
+    handler: async ({ input, client, withRooms, all }) => {
         console.log("Message received from client:", input);
         //socketModule.io.emit('message', input); // Broadcast the message to all clients
+        await all.broadcast("new_messages", {msg: input[0]});//Unsupported event new_messages
     }
 });
+
+
 /** @see https://socket.io/docs/v4/server-options/ */
 export const socketModule = {
     config,
     actionsFactory,
-    actions: [onPing, onChat, onMessage],
+    actions: [onPing, onMessage],
     io:new Server({ 
         cors: {
           origin: "*", 
