ce72a3296370d7fc24eebfb31cf9664f5779f359
ethansnow <ethansnow2012@gmail.com>
2024-05-27 10:08:43 +0800

    pov: saga-socket part 1

---
+++ apps/redux_saga-nextjs-app/src/app/socketPage/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import MySocket from "@/components/MySocket"
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <MySocket />
+    </div>
+  )
+}
+
+export default Page
---
+++ apps/redux_saga-nextjs-app/src/components/MySocket.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React, { useEffect, useRef } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import type { RootState } from "@/store"
+
+export default function () {
+  return (
+    <div>
+      <div>This is socket page.</div>
+    </div>
+  )
+}
